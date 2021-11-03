cfbe90d63440ed1085bdd03559f03db079e60df3
Josh Arroyo <josh@prismatec.co>
2021-11-03 10:48:05 -1000

    Change message sort implementation from backend to frontend

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,15 +6,24 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
+  const sortedMessages = messages.sort((a, b) => {
+    return new Date(a.createdAt) - new Date(b.createdAt);
+  });
+
   return (
     <Box>
-      {messages.map((message) => {
+      {sortedMessages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          <OtherUserBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+          />
         );
       })}
     </Box>
