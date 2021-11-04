ab24cc3e3894933c2ca3592011c54e3acd71a86e
Josh Arroyo <josh@prismatec.co>
2021-11-03 17:35:32 -1000

    Remove client side message sort

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,13 +6,9 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
-  const sortedMessages = messages.sort((a, b) => {
-    return new Date(a.createdAt) - new Date(b.createdAt);
-  });
-
   return (
     <Box>
-      {sortedMessages.map((message) => {
+      {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
